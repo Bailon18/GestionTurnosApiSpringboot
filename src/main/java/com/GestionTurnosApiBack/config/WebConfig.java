17f6f7b9cd021a561e5d3528321ca0fb219005a7
Bailon18 <paucarmontesbailon@gmail.com>
2024-05-13 20:11:05 -0500

    se actualizo

--- src/main/java/com/GestionTurnosApiBack/config/WebConfig.java
+++ src/main/java/com/GestionTurnosApiBack/config/WebConfig.java
@@ -7,23 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    /*
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/llamar")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
-
-        registry.addMapping("/listado")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
-
-        registry.addMapping("/atenciones/**")
-                .allowedOrigins("http://localhost:4200")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowCredentials(true);
-    }*/
+    }
 }
